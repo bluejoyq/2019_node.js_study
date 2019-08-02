aa32c19912fc8e54844cc45cf4852814231179a8
bluejoyq <bluejoyq@naver.com>
2019-08-02 19:05:08 +0900

    2019-08-02 lecture2 assignment error fix

--- 2_functions/2.js
+++ 2_functions/2.js
@@ -1,9 +1,16 @@
-function foo(a, b, callback)
-{
-    console.log("i'm foo(), a:%s, b:%s", a, b);
-    callback('success call foo()');
-}
+const list=[1,2,3,4,5,6,7,8,9,10];
 
-foo(1, 2, function(msg){
-    console.log(msg);
+const callbackExample=(items,callback)=>{
+    setTimeout(
+        ()=>
+        {   
+            let sum=0;
+            items.map(item=>sum+=item);
+            callback(sum);
+        }
+    ,0);
+};
+callbackExample(list,function(result){
+    console.log(result);
 });
+console.log('first');
