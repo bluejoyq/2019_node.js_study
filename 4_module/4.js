c0d979360514fb6ab705eeb4c8c219c3314f3b33
bluejoyq <bluejoyq@naver.com>
2019-08-15 13:44:03 +0900

    2019-08-15 issue #2 fixed

--- 4_module/4.js
+++ 4_module/4.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
+const calc = require('./opr');
+
 fs.readFile('./4_module/input.txt',(err,data)=>{
     if (err) throw err;
     let str = (data.toString()).split(',');
     str[0] = Number(str[0]);
     str[2] = Number(str[2]);
     let content;
-    if (str[1] =="+") {content = str[0]+str[2];}
-    if (str[1] =="-") {content = str[0]-str[2];}
-    if (str[1] =="*") {content = str[0]*str[2];}
-    if (str[1] =="/") {content = str[0]/str[2];}
+    if (str[1] =="+") {content = calc.add(str[0],str[2]);}
+    if (str[1] =="-") {content = calc.subtract(str[0],str[2]);}
+    if (str[1] =="*") {content = calc.multiply(str[0],str[2]);}
+    if (str[1] =="/") {content = calc.divide(str[0],str[2]);}
     fs.writeFile('./4_module/output.txt',content,(err)=>{
         if(err) throw err;
     });
